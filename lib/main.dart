74d9bd6ecbb6024105388f23af81057eaaed3925
Ayush Naik <ayush27naik27@gmail.com>
2023-05-21 22:59:56 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,78 @@
+
+
+import 'package:flutter/material.dart';
+import 'package:shop_app/screens/main_screen.dart';
+
+import './dummy_data.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      drawer: Drawer(
+          child: Column(
+        children: [
+          Container(
+            height: 15,
+            width: double.infinity,
+            color: Colors.pink.shade400,
+          ),
+          drawer_item(Icons.production_quantity_limits_outlined, 'All Items'),
+          drawer_item(Icons.settings_accessibility_rounded, 'Settings'),
+        ],
+      )),
+      appBar: AppBar(
+        title: Text('Shop Ki List'),
+        backgroundColor: Colors.pink,
+      ),
+      body: GridView.builder(
+        padding: const EdgeInsets.all(10),
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            childAspectRatio: 3 / 2,
+            crossAxisSpacing: 10,
+            mainAxisSpacing: 10),
+        itemCount: items.length,
+        itemBuilder: (context, index) {
+          return GridTile(
+            child: Image.network(items[index].imageUrl),
+
+            
+            
+          );
+        },
+      ),
+    );
+  }
+
+  Container drawer_item(final IconData icon, final String title) {
+    return Container(
+      margin: EdgeInsets.all(10),
+      child: Row(
+        children: [
+          Icon(icon),
+          SizedBox(
+            width: 10,
+          ),
+          Text(title),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/screens/main_screen.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        drawer: Drawer(),
+        appBar: AppBar(
+          backgroundColor: Colors.white24,
+        ),
+        body: Container(
+          padding: EdgeInsets.fromLTRB(10, 20, 0, 10),
+          height: 70,
+          color: Colors.amber,
+          alignment: Alignment.topCenter,
+          child: Row(
+            children: [
+              Text(
+                'Categories',
+                style: TextStyle(fontSize: 30),
+              ),
+              SizedBox(width: 28),
+              Container(
+                width: 225,
+                height: 40,
+                padding: EdgeInsets.all(4),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(30),
+                  color: Color.fromRGBO(248, 223, 243, 100),
+                ),
+                child: Row(children: [
+                  Icon(Icons.search_outlined),
+                  SizedBox(width: 5),
+                  Expanded(
+                    child: TextField(
+                      decoration:
+                          InputDecoration(labelText: 'Search Any Product'),
+                          
+                    ),
+                  ),
+                ]),
+              ),
+            ],
+          ),
+        ));
+  }
+}

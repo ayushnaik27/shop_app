5a365eacc3756505dd871b270c285528e12f9baa
Ayush Naik <ayush27naik27@gmail.com>
2023-06-03 14:55:08 +0530

    Product DetailScreenAdded

--- lib/screens/product_detail_screen.dart
+++ lib/screens/product_detail_screen.dart
@@ -15,6 +15,37 @@ class ProductDetailPage extends StatelessWidget {
       appBar: AppBar(
         title: Text(loadedProduct.title),
       ),
+      body: Column(children: [
+        Container(
+          height: 300,
+          width: double.infinity,
+          child: Image.network(
+            loadedProduct.imageUrl,
+            fit: BoxFit.cover,
+          ),
+        ),
+        Container(
+          padding: EdgeInsets.all(8),
+          child: Text(
+            loadedProduct.title,
+            style: TextStyle(color: Colors.amber, fontSize: 30),
+          ),
+        ),
+        Container(
+          padding: EdgeInsets.all(8),
+          child: Text(
+            loadedProduct.description,
+            style: TextStyle(color: Colors.grey, fontSize: 10),
+          ),
+        ),
+        Container(
+          padding: EdgeInsets.all(8),
+          child: Text(
+            '₹${loadedProduct.price}',
+            style: TextStyle(color: Colors.grey, fontSize: 10),
+          ),
+        ),
+      ]),
     );
   }
 }

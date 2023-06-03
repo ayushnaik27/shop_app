e0963b325983e06a9b8fc590dd7eeccea74337cf
Ayush Naik <ayush27naik27@gmail.com>
2023-06-04 01:37:23 +0530

    Expandingg wala feature added...

--- lib/widgets/order_item.dart
+++ lib/widgets/order_item.dart
@@ -1,13 +1,21 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
 import '../providers/order_provider.dart' as ord;
 
-class OrderItem extends StatelessWidget {
+class OrderItem extends StatefulWidget {
   final ord.OrderItem order;
 
   OrderItem(this.order);
 
+  @override
+  State<OrderItem> createState() => _OrderItemState();
+}
+
+class _OrderItemState extends State<OrderItem> {
+  bool _isExpanded = false;
   @override
   Widget build(BuildContext context) {
     return Card(
@@ -15,13 +23,32 @@ class OrderItem extends StatelessWidget {
       child: Column(
         children: [
           ListTile(
-            title: Text('₹${order.amount}'),
-            subtitle: Text(DateFormat('dd/MM/yyyy hh:mm').format(order.time)),
+            title: Text('₹${widget.order.amount.toStringAsFixed(2)}'),
+            subtitle:
+                Text(DateFormat('dd/MM/yyyy hh:mm').format(widget.order.time)),
             trailing: IconButton(
-              icon: Icon(Icons.expand_more),
-              onPressed: () {},
+              icon: Icon(_isExpanded ? Icons.expand_less : Icons.expand_more),
+              onPressed: () {
+                setState(() {
+                  _isExpanded = !_isExpanded;
+                });
+              },
             ),
           ),
+          if (_isExpanded)
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 15, vertical: 4),
+              height: min(widget.order.cartitem.length * 15.0 + 20, 100),
+              child: ListView(
+                  children: widget.order.cartitem
+                      .map((item) => Row(
+                            children: [
+                              Text(item.title),
+                              Text('${item.quantity}x ₹${item.price}')
+                            ],
+                          ))
+                      .toList()),
+            ),
         ],
       ),
     );
